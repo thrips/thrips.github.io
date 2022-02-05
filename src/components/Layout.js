00ed252c13262f45160a3fa6c65b3477f3ac505f
thrips <78996355+thrips@users.noreply.github.com>
2022-02-04 16:30:11 -0800

    Remove backgroud color

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,7 +10,6 @@ const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-        background-color: 0xcae3eb;
     }
 `
 const Content = styled.div`
